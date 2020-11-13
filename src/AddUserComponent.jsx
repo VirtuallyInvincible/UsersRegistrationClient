cc826c912babe62b6e905a7aa2ce636d7abce9a6
Shai Mahfud <shai_mahfud@hotmail.com>
2020-11-13 14:35:37 +0200

    Added the component for entering the new user information. It encapsulates the logic for adding a new user.

---
+++ src/AddUserComponent.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class AddUserComponent extends React.Component {
+    constructor() {
+	    super();
+	
+		this.add = this.add.bind(this);
+    }
+  
+    render() {
+		return (
+		// TODO: This div should be a form that validates the input fields. If not all fields have been entered, the request should
+		// TODO: If the ID entered already exists in the database, an error message should be displayed
+			<div>
+			  <input ref='id' type='text' placeholder='ID' />
+			  <br />
+			  <input ref='name' type='text' placeholder='Name' />
+			  <br />
+			  <input ref='age' type='text' placeholder='Age' />
+			  <br />
+			  <input ref='jobTitle' type='text' placeholder='Job Title' />
+			  <br />
+			  <button onClick={this.add}>Add</button>
+		    </div>
+		);
+    }
+  
+    add() {
+		var id = ReactDOM.findDOMNode(this.refs.id).value;
+		var name = ReactDOM.findDOMNode(this.refs.name).value;
+		var age = ReactDOM.findDOMNode(this.refs.age).value;
+		var jobTitle = ReactDOM.findDOMNode(this.refs.jobTitle).value;
+		var requestOptions = {
+		    method: 'POST',
+		    headers: { 'Content-Type': 'application/json' },
+		    body: JSON.stringify({'id': id, 'Name': name, 'Age': age, 'Job Title': jobTitle})
+		};
+		fetch(`http://localhost:27017/user`, requestOptions)
+		    .catch(console.log);
+		
+		this.props.finish();
+    }
+}
+export default AddUserComponent;
--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,50 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import AddUserComponent from './AddUserComponent.jsx'
 
 
 // TODO: Moving to the cloud will help in scaling up the application as well as enabling remote clients (outside localhost) to send requests.
 
 
 class App extends React.Component {
-  constructor() {
-	super();
+    constructor() {
+		super();
+		
+		this.state = {
+		    data: 'No data available',	// TODO: Fetch data from database by calling the get all users API and displaying the results.
+		    viewAddUserComponent: false
+		};
+		
+		this.setViewAddUserComponent = this.setViewAddUserComponent.bind(this);
+		this.refresh = this.refresh.bind(this);
+		
+		this.refresh();
+    }
+  
+    render() {
+        return (
+			<div>
+			  <div>
+				{this.state.viewAddUserComponent ? 
+				  <AddUserComponent finish={() => this.setViewAddUserComponent(false)} /> : 
+				  <button onClick={() => this.setViewAddUserComponent(true)}>Add New User</button>}
+			  </div>
+			  <div>
+				<label>{this.state.data}</label>
+			  </div>
+			</div>
+        );
+    }
+  
+    setViewAddUserComponent(isVisible) {
+	    this.setState({ viewAddUserComponent: isVisible });
+	    if (!isVisible) {
+		    this.refresh();
+	    }
+    }
 	
-	this.state = {
-	  data: 'No data available'	// TODO: Fetch data from database by calling the get all users API and displaying the results.
-	};
-	
-	this.createNewUser = this.createNewUser.bind(this);
-  }
-  createNewUser() {
-	// TODO : Pop up a form to enable the user to enter the details of the new user, then forward the input to the call.
-	const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({"id": 3, "Name": "Shai Mahfud", "Age": 34, "Job Title": "Software Engineer"})
-    };
-	fetch(`http://localhost:27017/user`, requestOptions)
-	  .then(res => res.json())
-	  .then((data) => {
-	    this.setState({ data: data.Message });
-	  })
-	  .catch(console.log);
-	// TODO: Update this.state to also show the new user by calling the get all users API and displaying the results.
-	// TODO: Create a new record with the users details on the screen.
-  }
-  render() {
-    return (
-      <div>
-		<button onClick={this.createNewUser}>Add New User</button>
-		<br />
-		<label>{this.state.data}</label>
-      </div>
-    );	// TODO: Should be a dynamic list populated by the users with a delete button for each record to enable deletion.
-  }
+	refresh() {
+		// TODO: Fetch data from server and refresh the component that displays the results
+		// TODO: Should be a dynamic list populated by the users with a delete button for each record to enable deletion.
+	}
 }
 export default App;
