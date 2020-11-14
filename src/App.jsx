ae60ce052a1c89cc2ab331a2648d005117f5076c
Shai Mahfud <shai_mahfud@hotmail.com>
2020-11-14 10:35:46 +0200

    The data from the database is now fetched and displayed to the screen. The data is refreshed when a new user is added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import AddUserComponent from './AddUserComponent.jsx'
+import Constants from './constants.js'
 
 
 // TODO: Moving to the cloud will help in scaling up the application as well as enabling remote clients (outside localhost) to send requests.
 
 
 class App extends React.Component {
-    constructor() {
+	constructor() {
 		super();
 		
 		this.state = {
-		    data: 'No data available',	// TODO: Fetch data from database by calling the get all users API and displaying the results.
+		    data: Constants.NO_DATA_TEXT,
 		    viewAddUserComponent: false
 		};
 		
 		this.setViewAddUserComponent = this.setViewAddUserComponent.bind(this);
 		this.refresh = this.refresh.bind(this);
+		this.updateUsersData = this.updateUsersData.bind(this);
 		
 		this.refresh();
     }
@@ -43,7 +45,13 @@ class App extends React.Component {
     }
 	
 	refresh() {
-		// TODO: Fetch data from server and refresh the component that displays the results
+		fetch('http://localhost:27017/listUsers')
+			.then(response => response.json())
+			.then(data => this.updateUsersData(data.Data));
+	}
+	
+	updateUsersData(newData) {
+		this.setState({ data: newData.length == 0 ? Constants.NO_DATA_TEXT : newData });
 		// TODO: Should be a dynamic list populated by the users with a delete button for each record to enable deletion.
 	}
 }
